ee500852bd4e8ae1e72df0c1cf3d4c569e96445f
Aslam <aslam1qqqq@gmail.com>
2021-06-04 18:38:01 +0530

    First Commit
    completed all exersices

---
+++ LabExcersizesToSubmit/src/com/lab/bookone/CheckEvenOdd.java
@@ -0,0 +1,17 @@
+// EXP 6
+
+package com.lab.bookone;
+
+public class CheckEvenOdd {
+
+	public static void main(String[] args) {
+		
+		int num = 20;
+		if (num % 2 == 0)
+			System.out.println("Entered number is even");
+		else
+			System.out.println("Entered number is odd");
+		
+	}
+
+}
