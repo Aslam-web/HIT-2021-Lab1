cd5aaeed92b77d7bad605ae3404f71949f508810
Aslam <aslam1qqqq@gmail.com>
2021-07-09 01:08:51 +0530

    Week 5 Lab
    completed excersizes 1-5
    
    Modified files:
    	1. ThreadEvenOdd.java
    	2. ThreadDisplayMessage.java
    	3. ThreadInventory.java
    	4. ThreadDigitalClock.java
    	5. ThreadStart.java

---
+++ LabExcersizesToSubmit/src/com/lab/bookfive/ThreadDigitalClock.java
@@ -0,0 +1,22 @@
+package com.lab.bookfive;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ThreadDigitalClock {
+
+	public static void main(String[] args) {
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
+
+		new Thread(() -> {
+			while (true) {
+				System.out.println(simpleDateFormat.format(new Date()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
+			}
+		}).start();
+	}
+}
---
+++ LabExcersizesToSubmit/src/com/lab/bookfive/ThreadEvenOdd.java
@@ -0,0 +1,30 @@
+// Q1
+
+package com.lab.bookfive;
+
+public class ThreadEvenOdd {
+
+	public static void main(String[] args) {
+		new Thread(new EvenOdd(), "even").start();
+		new Thread(new EvenOdd(), "odd").start();
+	}
+
+}
+
+class EvenOdd extends Thread {
+	@Override
+	public void run() {
+		String name = Thread.currentThread().getName();
+		if (name.equalsIgnoreCase("even")) {
+			for (int i = 0; i < 100; i += 2) {
+//				System.out.println(i%2==0 ? "": i);
+				System.out.println(name+" : "+i);
+			}
+
+		} else if (name.equalsIgnoreCase("odd")) {
+			for (int i = 1; i < 100; i += 2) {
+				System.out.println(name+"  : "+i);
+			}
+		}
+	}
+}
